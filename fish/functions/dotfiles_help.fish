ece0a7e2ef29750732f67f054b0e63c29e6c4f27
Kaleb Alanis <kalebalanisdev@gmail.com>
2025-01-23 23:16:29 -0600

    Add dotfiles_help command

---
+++ fish/functions/dotfiles_help.fish
@@ -0,0 +1,5 @@
+function dotfiles_help
+echo "# Setup guide for Dotfiles"
+echo "First, make sure to have a good terminal emulator, ie WezTerm, Kitty, etc"
+echo "Next, get a good nerd font. You prefer CaskaydiaCove Nerd Font"
+end
