9cd84ddab76bb4273e1d31fd213c6325cbe58f9e
Kaleb Alanis <kalebalanisdev@gmail.com>
2025-04-19 12:50:09 -0500

    update nvim config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -33,8 +33,7 @@ vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 
 
-
-
+vim.keymap.set("n", "<leader><leader>", "<CMD>Oil<CR>", { desc = "Open parent directory"})
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -48,3 +47,11 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+
+
