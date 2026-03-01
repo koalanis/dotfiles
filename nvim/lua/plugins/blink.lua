ec32758922d982b7c9a3e89327d1af36e0f70101
Kaleb Alanis <kalebalanisdev@gmail.com>
2026-03-01 12:59:16 -0600

    update nvim config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -3,6 +3,16 @@ return {
     version = "*",
     opts = {
       keymap = { preset = "default" },
+      completion = {
+        list  = {
+          selection = {
+            preselect = true
+          }
+        },
+        menu = {
+          border = "rounded"  
+        }
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
